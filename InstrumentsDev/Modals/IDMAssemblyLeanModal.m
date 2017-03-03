93a7c6b14229c6b971545f95e90d600a3f615acd
szzc <yangfu132@163.com>
2017-03-03 15:30:29 +0800

    没用的逻辑

--- InstrumentsDev/Modals/IDMAssemblyLeanModal.m
+++ InstrumentsDev/Modals/IDMAssemblyLeanModal.m
@@ -13,17 +13,35 @@ int Add(int arg1, int arg2)
     return arg1 + arg2;
 }
 
+struct myrd{
+    int i1;
+    int i2;
+    int i3;
+};
+
+struct myrd myfunc(){
+    struct myrd r1;
+    r1.i1 = 1;
+    r1.i2 = 2;
+    r1.i3 = r1.i1 + r1.i2;
+    NSLog(@"%d,%d,%d",r1.i1,r1.i2,r1.i3);
+    return r1;
+}
+
+void mainTest()
+{
+    struct myrd r;
+    r = myfunc();
+    NSLog(@"%d,%d,%d",r.i1,r.i2,r.i3);
+}
+
 @implementation IDMAssemblyLeanModal
 - (instancetype) init
 {
     self = [super init];
     if (self)
     {
-        int nResult = Add(1,2);
-        if (nResult)
-        {
-            
-        }
+        mainTest();
     }
     
     return self;
